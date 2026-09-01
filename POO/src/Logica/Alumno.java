ffe791f872c41bb71b1ac144e00f4287e68b0742
DamianDubra <118073872+DamianDubra@users.noreply.github.com>
2026-09-01 18:42:10 +0000

    .

--- POO/src/Logica/Alumno.java
+++ POO/src/Logica/Alumno.java
@@ -4,4 +4,31 @@ public class Alumno{
     int id;
     String nombre;
     String apellido;
+
+    public Alumno() {
+    }
+
+    public Alumno(String apellido, int id, String nombre) {
+        this.apellido = apellido;
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+
+
+    public void mostrarNombre(){
+
+        System.out.println("Hola soy un alumno y se decir mi nombre"+ nombre);
+    }
+    public void saberAprobado(double calificacion){
+
+        if (calificacion>=6){
+            System.out.println("Aprobe la materia");
+
+        }
+        else{
+            System.out.println("Reprobe");
+        }
+
+    }
 }
