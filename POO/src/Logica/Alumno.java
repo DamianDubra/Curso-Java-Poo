641f2a5164c9b9d34042e7c3102769921b505a86
DamianDubra <118073872+DamianDubra@users.noreply.github.com>
2026-09-01 19:29:52 +0000

    getter y setters

--- POO/src/Logica/Alumno.java
+++ POO/src/Logica/Alumno.java
@@ -14,12 +14,12 @@ public class Alumno{
         this.nombre = nombre;
     }
 
-
-
+    
     public void mostrarNombre(){
 
         System.out.println("Hola soy un alumno y se decir mi nombre"+ nombre);
     }
+
     public void saberAprobado(double calificacion){
 
         if (calificacion>=6){
@@ -30,5 +30,29 @@ public class Alumno{
             System.out.println("Reprobe");
         }
 
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
     }
 }
--- POO/src/Logica/POO.java
+++ POO/src/Logica/POO.java
@@ -4,5 +4,21 @@ public class POO {
 
     public static void  main (String[] args) {
         
+
+        Alumno alu1 = new Alumno ();
+        Alumno alu2 = new Alumno ("Rodriguez", 15 ,"Raul");
+
+        System.out.println("El nmbre del alumno es"+ alu2.getNombre());
+        System.out.println("El nmbre del apellido es"+ alu2.getApellido());
+        System.out.println("El id del alumno es"+ alu2.getId());
+
+        alu1.setId(8);
+        alu1.setNombre("papa");
+        alu1.setApellido("mama");
+
+
+        System.out.println("El nmbre del alumno es"+ alu1.getNombre());
+        System.out.println("El nmbre del apellido es"+ alu1.getApellido());
+        System.out.println("El id del alumno es"+ alu1.getId());
     }
 }
